eba34f26ac9dc9dc1b2a98eb4bdc5affd90110da
odj110220 <981755795@qq.com>
2019-07-10 14:59:57 +0800

    ISS53继续开发添加了11111提交

--- src/main/java/com/yidu/HelloGit.java
+++ src/main/java/com/yidu/HelloGit.java
@@ -4,5 +4,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		//测试Git
 		System.out.println("Hello Git");
+		
+		
+		//--------修复bug后继续开发----------------
+		System.out.println("1111111111");
 	}
 }
